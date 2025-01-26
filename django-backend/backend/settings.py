6c752d54c7a5373007ef44b8ee914331e94a3c97
Thomas Campbell <trc527@gmail.com>
2025-01-25 18:56:16 -0600

    Inital setup and decision making

--- django-backend/backend/settings.py
+++ django-backend/backend/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Added Frameworks
+    'rest_framework',
+    # Project Frameworks
 ]
 
 MIDDLEWARE = [
